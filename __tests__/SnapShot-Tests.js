86b892e6a25a06c1500cf6c55f0d331bae867a1e
abdurrehman99 <syed.ebad1999@gmail.com>
2021-01-30 15:42:27 +0500

    snapshots tests for each UI component

---
+++ __tests__/SnapShot-Tests.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import {
+  ActivityIndicator,
+  AlbumCard,
+  Backdrop,
+  Header,
+  Heading,
+  ImageThumbnail,
+  ListItem,
+  SubHeading,
+  SwipeButton,
+} from '../src/components';
+import {create} from 'react-test-renderer';
+
+const ActivityIndicatorTree = create(<ActivityIndicator />),
+  AlbumCardTree = create(<AlbumCard />),
+  BackdropTree = create(<Backdrop />),
+  HeaderTree = create(<Header />),
+  HeadingTree = create(<Heading />),
+  ImageThumbnailTree = create(<ImageThumbnail />),
+  ListItemTree = create(<ListItem />),
+  SubHeadingTree = create(<SubHeading />),
+  SwipeButtonTree = create(<SwipeButton />);
+
+test('ActivityIndicator Snapshot', () => {
+  expect(ActivityIndicatorTree).toMatchSnapshot();
+});
+
+test('AlbumCard Snapshot', () => {
+  expect(AlbumCardTree).toMatchSnapshot();
+});
+
+test('Backdrop Snapshot', () => {
+  expect(BackdropTree).toMatchSnapshot();
+});
+
+test('Backdrop Snapshot', () => {
+  expect(HeaderTree).toMatchSnapshot();
+});
+
+test('Heading Snapshot', () => {
+  expect(HeadingTree).toMatchSnapshot();
+});
+
+test('ImageThumbnail Snapshot', () => {
+  expect(ImageThumbnailTree).toMatchSnapshot();
+});
+
+test('ListItem Snapshot', () => {
+  expect(ListItemTree).toMatchSnapshot();
+});
+
+test('SubHeading Snapshot', () => {
+  expect(SubHeadingTree).toMatchSnapshot();
+});
+
+test('SwipeButton Snapshot', () => {
+  expect(SwipeButtonTree).toMatchSnapshot();
+});
