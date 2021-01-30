8cf881f15cc5bc843be73071aff92cb6b54851ef
abdurrehman99 <syed.ebad1999@gmail.com>
2021-01-30 19:03:47 +0500

    unused variable removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
-
 const middleWare = [thunk];
 
 const store = createStore(
-	rootReducer,
-	{},
-	compose(applyMiddleware(...middleWare)),
+  rootReducer,
+  {},
+  compose(applyMiddleware(...middleWare)),
 );
 
 export default store;
